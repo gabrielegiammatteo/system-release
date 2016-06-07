5b03cccaac7c4a35445c4686c6505ea6cf514224
gabrielegiammatteo <gabriele.giammatteo@eng.it>
2016-06-07 15:33:58 +0200

    impror refactore

--- test/java/org/gcube/tools/report/Test.java
+++ test/java/org/gcube/tools/report/Test.java
@@ -4,15 +4,6 @@ import it.eng.d4s.sa3.util.Version;
 
 import java.io.File;
 import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
